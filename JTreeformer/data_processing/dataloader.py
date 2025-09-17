23c7b3b9834dfefdffd202539c00c4c509588c9d
XCX-scholar <162406134+XCX-scholar@users.noreply.github.com>
2025-09-17 20:42:16 +0800

    Update dataloader.py

--- JTreeformer/data_processing/dataloader.py
+++ JTreeformer/data_processing/dataloader.py
@@ -4,8 +4,7 @@ from torch.utils.data import DataLoader
 from torch_geometric.data import Batch
 import torch.nn.functional as F
 
-from data_processing.dataset import TreeDataset, LatentDataset
-
+from data_processing.dataset import TreeDataset, LatentDataset, PredictorDataset
 
 def collate_fn(batch):
     """
@@ -17,8 +16,17 @@ def collate_fn(batch):
     padded_x = pad_sequence(x_list, batch_first=True, padding_value=0)
 
     pyg_batch.x_dense = padded_x
-    pyg_batch.padding_mask = (padded_x == 0)
-    pyg_batch.padding_mask_nodes = (padded_x[:, :-1] == 0)
+    seq_lengths = torch.tensor([len(data.x) for data in batch], dtype=torch.long, device=padded_x.device)
+    max_len = padded_x.size(1)
+    range_tensor = torch.arange(max_len, device=padded_x.device).expand(len(batch), -1)
+    pyg_batch.padding_mask = range_tensor >= seq_lengths.unsqueeze(1)
+
+    # num_nodes_list = [data.num_nodes for data in batch]
+    # num_nodes = torch.tensor(num_nodes_list, device=padded_x.device)
+    # max_nodes = padded_x.size(1)
+    # range_tensor = torch.arange(max_nodes, device=padded_x.device).expand(len(batch), -1)
+    # virtual_token_indices = (num_nodes - 1).unsqueeze(1)
+    # pyg_batch.padding_mask_nodes = range_tensor >= virtual_token_indices
 
     for key in ['hs', 'layer_number', 'degree', 'parent_pos']:
         feat_list = [getattr(data, key) for data in batch]
@@ -52,4 +60,9 @@ def create_vae_dataloader(dataset_path: str, batch_size: int, shuffle: bool = Tr
 def create_ddpm_dataloader(dataset_path: str, batch_size: int, shuffle: bool = True, num_workers: int = 0):
     """ Creates a DataLoader for the DDPM's LatentDataset. """
     dataset = LatentDataset(latent_path=dataset_path)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+def create_predictor_dataloader(data_path: str, batch_size: int, targets_config: dict, shuffle: bool = True, num_workers: int = 0):
+    """ Creates a DataLoader for the Predictor's PredictorDataset. """
+    dataset = PredictorDataset(data_path=data_path, targets_config=targets_config)
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
